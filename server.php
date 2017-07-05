da3d0c94e453cb2f7a67ad01be513d300ecd2cdb
Virchar Caballero Bautista <bautistavirchar@users.noreply.github.com>
2017-07-05 18:59:46 +0800

    Update server.php

--- server.php
+++ server.php
@@ -4,15 +4,16 @@
     $query = 'select * from `heroes`';
     $heroes = mysqli_query($con,$query);
     $data_arr = array();
+    $count = 0;
     while($row = mysqli_fetch_assoc($heroes)){
+        $count++;
         $data_arr[] = $row;
     }
     $json_data = json_encode(
         array(
-            'recordsFiltered'=> count($heroes),
-            'recordsTotal'=> count($heroes),
+            'recordsFiltered'=> $count,
+            'recordsTotal'=> $count,
             'data'=> $data_arr
         ));
     echo $json_data;
-    //echo '<script>console.log('. $json_data .')</script>';
 ?>
